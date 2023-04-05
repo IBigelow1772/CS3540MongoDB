f5bfc28e098d1ebb8d03fa799587dd95a0c2bcc4
IBigelow1772 <105335931+IBigelow1772@users.noreply.github.com>
2023-04-05 16:40:24 -0600

    Add files via upload

---
+++ CrudRead.java
@@ -0,0 +1,23 @@
+import com.mongodb.client.*;
+import org.bson.Document;
+
+public class CrudRead {
+    public static void main(String[] args) {
+        String uri = "mongodb+srv://<escanaba1772>:<A8oiBV5KQHTgWVgb>@<cluster-url>?retryWrites=true&writeConcern=majority";
+        try (MongoClient mongoClient = MongoClients.create(uri)) {
+            // database and collection code goes here
+            MongoDatabase db = mongoClient.getDatabase("sample_guides");
+            MongoCollection<Document> coll = db.getCollection("planets");
+            // find code goes here
+            MongoCursor<Document> cursor = coll.find().iterator();
+            // iterate code goes here
+            try {
+                while (cursor.hasNext()) {
+                    System.out.println(cursor.next().toJson());
+                }
+            } finally {
+                cursor.close();
+            }
+        }
+    }
+}
